c33121f71e957d626e0d95b10fbdc3a4dc86f3ae
sethish <sethish@vmware.com>
2023-08-17 09:58:28 +0530

    updated last_updated field

--- gaming_board/views.py
+++ gaming_board/views.py
@@ -23,7 +23,7 @@ total_members = {
     "WS1 Intelligence and GEM": 54
 }
 
-last_updated_data = "Idea Submissions"
+last_updated_data = "Idea Submissions, 16th August 2023, 07:40 PM"
 
 firebaseConfig = {
     'apiKey': "AIzaSyD3V7p2Y897rZhIYVVUpBkUXQDpkqsJD_k",
